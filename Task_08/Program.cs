5b489ba9c31cdb6e75824483eabec9d78ace6405
Андрей <40392418+jokerjam-s@users.noreply.github.com>
2022-06-07 16:29:32 +0300

    add task 8

---
+++ Task_08/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+
+Задача 8: Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+
+*/
+
+int num, counter = 1;
+string strLine;
+
+Console.Write("Number: ");
+strLine = Console.ReadLine();
+num = int.Parse(strLine);
+
+if(num <=0){
+    Console.WriteLine("Error! Wrong number.");
+}
+else{
+    while(counter <= num){
+        if(counter%2 == 0){
+            Console.Write(counter + " ");
+        }
+        counter++;
+    }
+}
+ 
