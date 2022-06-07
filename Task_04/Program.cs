23f607d5fbbba32c93b4629cd42c1c59f3cf64ea
Андрей <40392418+jokerjam-s@users.noreply.github.com>
2022-06-07 14:22:02 +0300

    task 2, 4

---
+++ Task_04/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+    2 3 7 -> 7
+    44 5 78 -> 78
+    22 3 9 -> 22
+*/
+
+// реализация без массивов
+
+
+int n1, n2, n3;
+string strLine;
+
+Console.Write("Number 1: ");
+strLine = Console.ReadLine();
+n1 = int.Parse(strLine);
+
+Console.Write("Number 2: ");
+strLine = Console.ReadLine();
+n2 = int.Parse(strLine);
+
+Console.Write("Number 3: ");
+strLine = Console.ReadLine();
+n3 = int.Parse(strLine);
+
+
+if((n1 >= n2) && (n1 >= n3)){
+    Console.WriteLine("Max = "+ n1);
+}
+else if((n2 >= n1) && (n2 >= n3)){
+    Console.WriteLine("Max = "+ n2);
+}
+else{
+    Console.WriteLine("Max = "+ n3);
+}
